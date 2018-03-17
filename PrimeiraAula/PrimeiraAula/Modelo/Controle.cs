5b07849cf6705818b149f40026c2cb6edc539671
Vinterdanilo <36777793+Vinterdanilo@users.noreply.github.com>
2018-03-16 21:28:14 -0300

    Add files via upload
    projeto soma

---
+++ PrimeiraAula/PrimeiraAula/Modelo/Controle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PrimeiraAula.Modelo;
+
+
+namespace PrimeiraAula.Modelo
+{
+    public class Controle
+    {
+        /*atributos*/
+        public String num1;
+        public String num2;
+        public String op;
+        public String resposta;
+        public String mensagem;
+
+        /*classe Executar*/
+        public void Executar()
+        {
+            this.mensagem = "";
+
+            /*isntanciar a classe*/
+            validacao validacao = new validacao();
+            validacao.num1 = this.num1;
+            validacao.num2 = this.num2;
+            validacao.op = this.op;
+            validacao.Validar();
+            
+
+            if (validacao.mensagem.Equals(""))
+            {
+                /*isntanciar a classe*/
+                Calculos calculos = new Calculos();
+                calculos.n2 = validacao.n2;
+                calculos.n1 = validacao.n1;
+                calculos.op = this.op;
+                calculos.Calcular();
+                this.resposta = calculos.resposta;
+                
+            }
+            else
+            {
+                /*log de erro*/
+                this.mensagem = validacao.mensagem;
+            }
+
+        }
+    }
+}
---
+++ PrimeiraAula/PrimeiraAula/Modelo/Validacao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PrimeiraAula.Modelo;
+
+namespace PrimeiraAula.Modelo
+{
+   public class validacao
+    {
+       public String num1;
+       public String num2;
+       public Double n1;
+       public Double n2;
+       public String op;
+       public String mensagem;
+
+       /*metodo para se comunicar com os atributos*/
+       public void Validar()
+       {
+           this.mensagem = "";
+           try
+           {
+               this.n1 = Convert.ToDouble(this.num1);
+           }
+           catch (FormatException e)
+           {
+               this.mensagem = "Erro de converção 1 campo \n";
+           }
+
+           try
+           {
+               this.n2 = Convert.ToDouble(this.num2);
+           }
+           catch (FormatException e)
+           {
+               this.mensagem += "Erro de converção 2 campo";
+           }
+
+           
+           if (this.op.Equals("/") && this.num2.Equals("0"))
+           {
+               this.mensagem += "Não é possível divisão por zero";
+           }
+       }
+   }
+}

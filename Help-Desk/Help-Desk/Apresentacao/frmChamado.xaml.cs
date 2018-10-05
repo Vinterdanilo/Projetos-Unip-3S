7df30ea7c4682863381af5489ab56ac62c0ebea1
Vinterdanilo <36777793+Vinterdanilo@users.noreply.github.com>
2018-10-05 13:30:04 -0300

    PimDS4 - Help Desk
    Banco de dados - tela de login e cadastro!

---
+++ Help-Desk/Help-Desk/Apresentacao/frmChamado.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Help_Desk.Apresentacao
+{
+    /// <summary>
+    /// Interaction logic for frmChamado.xaml
+    /// </summary>
+    public partial class frmChamado : Window
+    {
+        public frmChamado()
+        {
+            InitializeComponent();
+
+            Modelo.Controle controle = new Modelo.Controle();
+            controle.PesquisaSolicitantePorID(Modelo.atbEstaticos.id_Usuario);
+            txbSolicitante.Text = Modelo.atbEstaticos.nome_Usuario;
+            txbData.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void btnLimpar_Click(object sender, RoutedEventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void btnSolicitar_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> dadoschamado = new List<string>();
+            dadoschamado.Add("0");
+            dadoschamado.Add(Modelo.atbEstaticos.id_Usuario.ToString());
+            dadoschamado.Add(cbxSetor.Text);
+            dadoschamado.Add(cbxPrioridade.Text);
+            dadoschamado.Add(txbData.Text);
+            dadoschamado.Add(txbDescriçao.Text);
+            Modelo.Controle controle = new Modelo.Controle();
+            controle.CadastraChamado(dadoschamado);
+            MessageBox.Show(controle.mensagem);
+            LimparCampos();
+        }
+        public void LimparCampos()
+        {
+            txbDescriçao.Clear();
+            cbxPrioridade.SelectedIndex = -1;
+            cbxSetor.SelectedIndex = -1;
+        }
+    }
+}
---
+++ Help-Desk/Help-Desk/Apresentacao/frmLogin.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Help_Desk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void txbEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            Modelo.Controle controle = new Modelo.Controle();
+            controle.Acessar(txbUsuario.Text, txbSenha.Text);
+            if(controle.mensagem.Equals(""))
+
+            { 
+                if (controle.possui)
+                {
+                    MessageBox.Show("logado com sucesso", "Entrando", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Apresentacao.frmChamado frmC = new Apresentacao.frmChamado();
+                    frmLogin.Close();
+                    frmC.ShowDialog();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("login nao encontrado, verifique login e senha", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show(controle.mensagem);
+            }
+
+        }
+    }
+}
